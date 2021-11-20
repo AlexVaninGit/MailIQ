74b9c75abfc8ba191172bd6191a60309d59cf31f
AlexVaninGit <vaninalexsandr98@gmail.com>
2021-11-20 20:24:34 +0300

    Added project

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+$(window).on('scroll', () => {
+    $(window).scrollTop() > 50
+        ? $('nav').addClass('bg-dark')
+        : $('nav').removeClass('bg-dark')
+})
+
+$('.navbar-toggler').on('click', () => {
+    $('nav').toggleClass('bg-dark')
+})
+
+$('.footer__link-top').on('click', (e) => {
+    $('html, body').animate({scrollTop: 0}, 600);
+    return false
+})
